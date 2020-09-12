5533d4084820c95025df0794d676df398a211c07
lucas santos <lucas.santos@dedtechsolutions.com.br>
2020-09-11 22:43:09 -0300

    libs iniciais

---
+++ App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { SafeAreaView, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <SafeAreaView>
+        <View>
+          <Text>teste</Text>
+        </View>
+      </SafeAreaView>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ README.md
@@ -0,0 +1,7 @@
+# desenvolve-kids
+
+App Desenvolvido sob demanda usando React Native, React Navigation, Redux, Redux Sagas, Redux Sauce, React Native Elements e outras libs.
+
+Desenvolvido por: Lucas Santos Mendonça da Silva
+
+contato: lucassms9@hotmail.com
